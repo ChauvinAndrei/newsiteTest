ed25ea9e4356c102049ac5b76667e5af707ad13c
ChauvinAndrei <Andrei.chauvin@hotmail.fr>
2023-08-08 22:57:23 +0200

    dsqd

--- src/Component/Menu/SousMenus/Account/Account.jsx
+++ src/Component/Menu/SousMenus/Account/Account.jsx
@@ -7,9 +7,11 @@ const Account = ( { isActive } ) => {
             <h2 className={`menu--title${isActive ? ' active': ''}`}>Account</h2>
 
             <div className="duContenu">
+                <p>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa laboriosam voluptate doloribus nesciunt necessitatibus aut quas nulla harum saepe, amet sint beatae et mollitia sapiente deleniti. Maxime provident sit id!
                 Quos deserunt quo officiis dicta sunt iure recusandae, beatae omnis ex eaque perspiciatis eius facere iusto delectus accusantium modi veniam unde nam. Laboriosam reiciendis vitae aspernatur hic, cum neque voluptatibus?
                 Eaque tenetur, ex fuga exercitationem facilis earum ut et libero doloribus reprehenderit sint atque quos, rerum est placeat repellendus ipsum. Sequi odio molestias quis ipsa nemo veniam quasi dignissimos cumque?
+                </p>
             </div>
         </Page>
     )
